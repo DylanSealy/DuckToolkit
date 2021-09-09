174d955578baf253d02c87723d5a3efd957407bd
Dylan Sealy <66305635+DylanSealy@users.noreply.github.com>
2021-09-09 18:03:59 +0200

    Merged PyPi/1.0.3

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ducktoolkit',
-    version='1.0.2',
+    version='1.0.3',
     author='Kevin Breen, James Hall',
     author_email='pip@ducktoolkit.com',
     description="USB Rubber Ducky Toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://ducktoolkit.com',
     license='GNU V3',
     zip_safe=False,
